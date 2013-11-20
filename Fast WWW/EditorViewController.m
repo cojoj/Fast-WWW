dcba74ca4400f66e71650003cc799e0969977661
Mateusz Zając <zajac.mateusz@icloud.com>
2013-11-20 23:40:34 +0100

    Added keboard scrolling

--- Fast WWW/EditorViewController.m
+++ Fast WWW/EditorViewController.m
@@ -23,6 +23,8 @@
     
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
     [self.view addGestureRecognizer:tap];
+    
+    [self addKeyboardObservers];
 }
 
 - (IBAction)savePage:(UIButton *)sender
@@ -41,6 +43,24 @@
 - (NSString *)applicationDocumentsDirectory
 {
     return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+}
+
+#pragma mark - Keyboard observers
+
+- (void)addKeyboardObservers
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWasShown:) name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeHidden:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)keyboardWasShown:(NSNotification*)notification {
+    NSDictionary* info = [notification userInfo];
+    CGSize keyboardSize = [[info objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue].size;
+    self.webisteTextView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - keyboardSize.height);
+}
+
+- (void)keyboardWillBeHidden:(NSNotification*)notification {
+    self.webisteTextView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
 }
 
 @end
